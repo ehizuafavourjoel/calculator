4ac9b97b3e500e860e89bd9f7c8f86097dfe66d4
Favour Ehizua <ehizuafavour@gmail.com>
2024-01-13 08:15:40 -1200

    My second calculator js type: Works better

--- main.js
+++ main.js
@@ -1,252 +1,191 @@
-//Output screen
-console.log(1 - 3 + 3)
-let questionOutput = document.querySelectorAll('.answerFlex textarea')[0];
-let answerOutput = document.querySelectorAll('.answerFlex textarea')[1];
-let flexInputView = document.querySelector('.answerFlex')
-const resetView = () => {
-    flexInputView.classList.remove('equalClickEvent')
-}
-const alone = () => {
-    if (questionOutput.innerHTML === '') {
-        console.log('jUST ALONE')
-        questionOutput.innerHTML = '0'
+
+document.addEventListener('readystatechange', (e) => {
+    if (e.target.readyState === 'complete') {
+        console.log(document.readyState)
+        initApp()
     }
-}
-
-function evaluate() {
-    if (questionOutput.innerHTML === '') {
-        answerOutput.innerHTML = ''
-
-    }
-    else {
-        // answerOutput.innerHTML = questionOutput.innerHTML
-        // try {
-        checkAndReplace()
-
-        // }
-        // catch (err) {
-        //     console.log(err)
-        //     answerOutput.innerHTML = 'Syntax Error'
-
-        // }
-    }
-
-}
-
-// function autoEqual() {
-//     //if includes × an ÷
-//     if (questionOutput.innerHTML.includes('÷')) {
-//         var modifiedAnswer = questionOutput.innerHTML.replace('÷', '/');
-//     }
-//     if (questionOutput.innerHTML.includes('×')) {
-//         modifiedAnswer = questionOutput.innerHTML.replace('×', '*')
-//     }
-//     if (questionOutput.innerHTML.includes('^')) {
-//         modifiedAnswer = questionOutput.innerHTML.replace('^', '**')
-//     }
-//     console.log(modifiedAnswer)
-
-//     // If lenght of answer is too long
-//     let autoAnswer = String(eval(modifiedAnswer));
-//     if (autoAnswer.length > 10) {
-//         console.log('too long')
-//         var plusLengthModified = eval(autoAnswer.slice(0, 14))
-//         answerOutput.innerHTML = eval(plusLengthModified)
-//     }
-//     else {
-//         console.log('lenght ok')
-//         console.log(answerLength)
-//         answerOutput.innerHTML = eval(modifiedAnswer)
-//     }
-
-// }
+})
 
 
-function checkAndReplace() {
-    let behindMath;
-    resetView()
+const initApp = () => {
+    let appStatus = 'calculator';
+    const container = document.querySelector('.container')
+    const navBtns = document.querySelectorAll('.heading span');
+    const questionInput = document.querySelector('.questionInput')
+    const answerInput = document.querySelector('.answerInput')
+    const allBtns = document.querySelectorAll('.btnContainer div input')
+    const delBtn = document.querySelector('#delBtn')
+    const historyContainer = document.querySelector('.historyContainer')
 
-    if (questionOutput.innerHTML.includes('÷')) {
-        console.log('has ÷')
-        behindMath = questionOutput.innerHTML.replaceAll('÷', '/')
-        console.log(eval(behindMath))
-        answerOutput.innerHTML = eval(behindMath)
-    }
-    if (questionOutput.innerHTML.includes('×')) {
-        console.log('has ×')
-        behindMath = questionOutput.innerHTML.replaceAll('×', '*')
-        console.log(behindMath);
-        answerOutput.innerHTML = eval(behindMath)
-    }
-    if (questionOutput.innerHTML.includes('−')) {
-        console.log('has −')
-        behindMath = questionOutput.innerHTML.replaceAll('−', '-')
-        console.log(behindMath);
-        answerOutput.innerHTML = eval(behindMath)
-    }
-    if (questionOutput.innerHTML.includes('+')) {
-        console.log('has +')
-        behindMath = questionOutput.innerHTML.replaceAll('+', '+')
-        console.log(behindMath);
-        answerOutput.innerHTML = eval(behindMath)
-    }
-    if (questionOutput.innerHTML.includes('^')) {
-        console.log('has ^')
-        behindMath = questionOutput.innerHTML.replaceAll('^', '**')
-        console.log(behindMath);
-        answerOutput.innerHTML = eval(behindMath)
+
+    //creatinghistory elements
+    function createAndAdd() {
+        let ul = document.createElement('ul')
+        let li1 = document.createElement('li')
+        let li2 = document.createElement('li')
+
+        li1.innerText = questionInput.value;
+        li2.innerText = '=' + answerInput.value;
+        ul.append(li1)
+        ul.append(li2)
+
+        historyContainer.prepend(ul)
+
     }
 
-    answerOutput.innerHTML = eval(7 * 9)
-    console.log(behindMath)
-    function checkAnsLength() {
-        if (String(eval(behindMath)).length > 14) {
-            let sliced = String(eval(behindMath)).slice(0, 14)
-            answerOutput.innerHTML = sliced
-        }
-    }
-    checkAnsLength()
-}
-
-// All calculation button
-//Row 1
-let row1 = document.querySelectorAll('.row1 div')
-
-//Row 2
-let row2 = document.querySelectorAll('.row2 div')
-
-
-//Row 3 
-let row3 = document.querySelectorAll('.row3 div')
-
-
-//Row 4
-let row4 = document.querySelectorAll('.row4 div')
-
-//row 5
-let row5 = document.querySelectorAll('.row5 div')
-
-
-for (let i = 0; i < 4; i++) {
-    //Row 1
-    row1[i].addEventListener('click', (e) => {
-        if (e.target) {
-            resetView()
-        }
-        if (i === 0) {
-            questionOutput.innerText = ''
-            answerOutput.innerText = ''
-        }
-        else if (i === 1) {
-
-            let sliced = questionOutput.innerHTML.slice(0, -1);
-            questionOutput.innerText += sliced;
-            answerOutput.innerHTML = sliced
-
-        }
-        else if (i === 2) {
-            if (questionOutput.innerHTML === '') {
-                answerOutput.innerHTML = ''
+    //For toggling Between calculator and History View
+    navBtns.forEach(navBtn => {
+        navBtn.addEventListener('click', () => {
+            if (navBtn.innerText === '=') {
+                container.classList.remove('historyArea')
             }
             else {
-                if (answerOutput.innerHTML === '') {
-                    answerOutput.innerHTML = Number(questionOutput.innerHTML) / 100
-                    questionOutput.innerHTML += '%'
+                container.classList.add('historyArea')
+
+            }
+        })
+    })
+
+    //Defining background maths values
+    let myBackMathValue = "";
+
+    //All the calculator Buttons Functionalities
+    //calculate function 
+
+    const signRules = (mysign, mysign2) => {
+        let miLastIndex = questionInput.value.charAt(questionInput.value.length - 1)
+        if (miLastIndex === '+' ||
+            miLastIndex === '-' ||
+            miLastIndex === '÷' ||
+            miLastIndex === '×' ||
+            miLastIndex === '.') {
+            let dummy = myBackMathValue.slice(0, -1)
+            let fullDummy1 = dummy + mysign;
+            var backFullDummy = dummy + mysign2
+            console.log(backFullDummy)
+            questionInput.value = fullDummy1
+            myBackMathValue = backFullDummy
+
+        }
+        else {
+            console.log('clean')
+            calculate(mysign, mysign2);
+        }
+
+
+
+
+    }
+    const calculate = (x, y = x) => {
+        if (questionInput.value === "" &&
+            myBackMathValue === '' && (x === '÷' || x === '+' || x === '%' || x === '×' || x === '-')) {
+            questionInput.value += 0 + x;
+            myBackMathValue += 0 + y
+        } else {
+            questionInput.value += x;
+            myBackMathValue += y
+
+        }
+        if (x === '9' || x === '8' || x === '7' || x === '6' || x === '5' || x === '4' || x === '3' || x === '2' || x === '1' || x === '0' || x === '.') {
+            // console.log(myBackMathValue)
+            answerInput.value = eval(myBackMathValue)
+        }
+
+    }
+    //for delBtn first 
+    delBtn.addEventListener('click', () => {
+        let mySliced = questionInput.value.slice(0, -1)
+        questionInput.value = mySliced;
+        myBackMathValue = mySliced
+        answerInput.value = myBackMathValue
+    })
+    allBtns.forEach(btn => {
+        /* if (questionInput.value === '') {
+        //     console.log('f')
+        //     document.querySelector('#clear').value = 'Ac'
+        // }
+        // else {
+        //     console.log('r')
+        //     document.querySelector('#clear').value = 'c'
+         }*/
+        btn.addEventListener('click', (e) => {
+            //equalTo rebounce event
+            document.querySelector('.answerFlex').classList.remove('equalClickEvent');
+
+            if (btn.value === 'c') {
+
+                questionInput.value = ''
+                answerInput.value = ''
+                myBackMathValue = ''
+            }
+            else if (btn.value === '%') {
+                console.log('%');
+                let myPercent = questionInput.value / 100;
+                questionInput.value = myPercent
+                answerInput.value = myPercent;
+                myBackMathValue = myPercent;
+                answerInput.value = myBackMathValue;
+            }
+            else if (btn.value === '÷') {
+                signRules('÷', '/')
+            }
+            else if (btn.value === '7') {
+                calculate('7');
+            }
+            else if (btn.value === '8') {
+                calculate('8');
+            }
+            else if (btn.value === '9') {
+                calculate('9');
+            }
+            else if (btn.value === '×') {
+                signRules('×', '*');
+            }
+            else if (btn.value === '4') {
+                calculate('4');
+            }
+            else if (btn.value === '5') {
+                calculate('5');
+            }
+            else if (btn.value === '6') {
+                calculate('6');
+            }
+            else if (btn.value === '−') {
+                signRules('-', '-');
+            }
+            else if (btn.value === '1') {
+                calculate('1');
+            }
+            else if (btn.value === '2') {
+                calculate('2');
+            }
+            else if (btn.value === '3') {
+                calculate('3');
+            }
+            else if (btn.value === '+') {
+                signRules('+');
+            }
+            else if (btn.value === '**') {
+                signRules('^');
+            }
+            else if (btn.value === '0') {
+                calculate('0');
+                if (questionInput.value === '0÷0') {
+                    answerInput.value = 'can'
                 }
-
-                else {
-                    checkAndReplace()
-                    answerOutput.innerHTML = Number(answerOutput.innerHTML) / 100
-                    questionOutput.innerHTML = answerOutput.innerHTML + '%'
-
+                if (questionInput.value.includes('÷0')) {
+                    answerInput.value = `can't divide by zero`
                 }
             }
-        }
-        else {
-            alone()
-            questionOutput.innerHTML += '÷'
+            else if (btn.value === '.') {
+                signRules('.')
+            }
+            else {
+                document.querySelector('.answerFlex').classList.add('equalClickEvent');
+                createAndAdd()
+            }
 
-        }
+        })
     })
 
-    //Row 2 
-    row2[i].addEventListener('click', () => {
-        if (i === 0) {
-            questionOutput.innerHTML += 7
-            evaluate()
-            // checkAndReplace()
-        }
-        else if (i === 1) {
-            questionOutput.innerHTML += 8
-            evaluate()
-            // checkAndReplace()
-        }
-        else if (i === 2) {
-            questionOutput.innerHTML += 9
-            evaluate()
-            // checkAndReplace()
-        }
-        else {
-            alone()
-            questionOutput.innerHTML += '×'
-
-        }
-    })
-
-    row3[i].addEventListener('click', () => {
-        if (i === 0) {
-            questionOutput.innerHTML += 4
-            evaluate()
-        }
-        else if (i === 1) {
-            questionOutput.innerHTML += 5
-            evaluate()
-        }
-        else if (i === 2) {
-            questionOutput.innerHTML += 6
-            evaluate()
-        }
-        else {
-            alone()
-            questionOutput.innerHTML += '−';
-        }
-    })
-
-    row4[i].addEventListener('click', () => {
-        if (i === 0) {
-            questionOutput.innerHTML += 1
-            evaluate()
-        }
-        else if (i === 1) {
-            questionOutput.innerHTML += 2
-            evaluate()
-        }
-        else if (i === 2) {
-            questionOutput.innerHTML += 3
-            evaluate()
-        }
-        else {
-            alone()
-            questionOutput.innerHTML += '+';
-        }
-    })
-
-    row5[i].addEventListener('click', () => {
-        if (i === 0) {
-            alone()
-            questionOutput.innerHTML += '^'
-        }
-        else if (i === 1) {
-            questionOutput.innerHTML += 0
-            evaluate()
-        }
-        else if (i === 2) {
-            questionOutput.innerHTML += '.'
-            evaluate()
-        }
-        else {
-            alone()
-            flexInputView.classList.add('equalClickEvent')
-        }
-    })
 }
